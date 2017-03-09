885e2e95251811e516ae595a299b770153bf3c22
Marta Delgado <marta.e.delgado@gmail.com>
2017-03-09 14:55:30 +0100

    posting secret with picture works

--- src/app/add-secret/add-secret.component.ts
+++ src/app/add-secret/add-secret.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { SessionService    } from '../services/session.service';
 import { SecretsService    } from '../services/secrets.service';
-import { Router } from '@angular/router';
+import { Router            } from '@angular/router';
+import { FileUploader      } from "ng2-file-upload";
 
 @Component({
   selector: 'app-add-secret',
@@ -9,6 +10,10 @@ import { Router } from '@angular/router';
   styleUrls: ['./add-secret.component.css']
 })
 export class AddSecretComponent implements OnInit {
+  uploader: FileUploader = new FileUploader({
+   url: `http://localhost:3000/api/secrets/add`
+    });
+
   secrets: any;
 
   newSecret = {
@@ -18,7 +23,9 @@ export class AddSecretComponent implements OnInit {
     description : " ",
     tips        : " ",
     when        : " "
-  }
+  };
+
+  feedback: string;
 
   constructor(
     private secret : SecretsService,
@@ -26,20 +33,30 @@ export class AddSecretComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    // console.log("new secret ", this.newSecret)
-    // this.secret.addSecret()
-    // .subscribe((secrets) => {
-    //   this.secrets = secrets
-    // });
+    this.uploader.onSuccessItem = (item, res) => {
+      this.feedback = JSON.parse(res).message;
+      console.log("success");
+    }
+
+    this.uploader.onErrorItem = (item, res, status, headers) => {
+      this.feedback = JSON.parse(res).message;
+      console.log("error");
+    }
+
   }
 
   addNewSecret() {
-    this.secret.addSecret(this.newSecret)
-    .subscribe((secrets) => {
-      this.secrets = secrets;
-      this.router.navigate(['/api/secrets']);
-    })
-  }
+    this.uploader.onBuildItemForm = (item, form) => {
+      form.append('where', this.newSecret.where);
+      form.append('location', this.newSecret.location);
+      form.append('what', this.newSecret.what);
+      form.append('description', this.newSecret.description);
+      form.append('tips', this.newSecret.tips);
+      form.append('when', this.newSecret.when)
 
+    };
+    this.uploader.uploadAll();
+    this.router.navigate(['/api/secrets']);
+  }
 
 }
--- src/app/services/secrets.service.ts
+++ src/app/services/secrets.service.ts
@@ -21,10 +21,4 @@ export class SecretsService {
       .map((res) => res.json());
   }
 
-  addSecret(newSecret) {
-    console.log("secret", newSecret);
-    return this.http.post(`${this.BASE_URL}/api/secrets/add`, newSecret)
-      .map((res) => res.json());
-  }
-
 }
